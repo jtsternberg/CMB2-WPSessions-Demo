beb142472162ac74db21729b3c068c6ad314c602
Justin Sternberg <justin@dsgnwrks.pro>
2015-05-24 14:16:00 -0400

    Add example-functions.php to the theme

--- themes/twentyfourteen-child-books/functions.php
+++ themes/twentyfourteen-child-books/functions.php
@@ -2,3 +2,12 @@
 /**
  * Twenty Fourteen -- BOOKS!
  */
+
+function twentyfourteenbooks_includes() {
+	$dir = get_stylesheet_directory();
+
+	include $dir . '/includes/cmb-fields.php';
+}
+
+// Include our includes!
+twentyfourteenbooks_includes();

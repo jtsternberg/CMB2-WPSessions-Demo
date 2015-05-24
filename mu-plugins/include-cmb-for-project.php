445445f8261db1e053dc8ecb6341142190102417
Justin Sternberg <justin@dsgnwrks.pro>
2015-05-24 14:00:07 -0400

    Remove our cmb-loaded test

--- mu-plugins/include-cmb-for-project.php
+++ mu-plugins/include-cmb-for-project.php
@@ -2,5 +2,3 @@
 
 // Include CMB2 globally for this site.
 require WPMU_PLUGIN_DIR . '/cmb2/init.php';
-
-wp_die( defined( 'CMB2_LOADED' ) ? 'CMB2 Loaded!' : 'uh oh! :(', __( 'CMB2 Check', 'wpsessions' ) );
